4b161693e99d6b559c117da3e03a514a523fdbd0
Sam Mullaney <sammull@udel.edu>
2025-10-05 16:45:12 -0400

    solved forms

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+interface MultipleChoiceQuestionProps {
+    expectedAnswer: string;
+    options: string[];
+}
 
 export function MultipleChoiceQuestion({
-    options,
     expectedAnswer,
-}: {
-    options: string[];
-    expectedAnswer: string;
-}): React.JSX.Element {
+    options,
+}: MultipleChoiceQuestionProps): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+
+    const isCorrect = choice === expectedAnswer;
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="multipleChoice">
+                <Form.Label>Select your answer:</Form.Label>
+                <Form.Select
+                    value={choice}
+                    onChange={(e) => {
+                        setChoice(e.target.value);
+                    }}
+                >
+                    {options.map((option) => {
+                        return (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        );
+                    })}
+                </Form.Select>
+            </Form.Group>
+            <div style={{ marginTop: "10px", fontSize: "20px" }}>
+                {isCorrect ? "✔️" : "❌"}
+            </div>
         </div>
     );
 }
